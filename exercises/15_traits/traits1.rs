d18ecf0aed753e6ca17077404d44dc5bdb71c378
Anil Vadlapatla <anil.s.vadlapatla@kp.org>
2025-09-06 11:14:11 -0700

    15_traits done

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -6,11 +6,14 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(self) -> Self {
+        format!("{self}Bar")
+    }
 }
 
 fn main() {
     let s = String::from("Foo");
-    let s = s.append_bar();
+    let s = s.append_bar().append_bar();
     println!("s: {s}");
 }
 
--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -10,13 +10,31 @@ struct OtherSoftware;
 impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
+struct MITSoftware;
+impl Licensed for MITSoftware {
+    fn licensing_info(&self) -> String {
+        "MIT License".to_string()
+    }
+}
+
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+fn compare_license_types(software1: impl Licensed, software2: impl Licensed) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
 
 fn main() {
     // You can optionally experiment here.
+    let cmp = match compare_license_types(SomeSoftware, OtherSoftware) {
+        true => "yes",
+        false => "no",
+    };
+    println!("Do both softwares have the same license? {cmp}");
+
+    let cmp = match compare_license_types(SomeSoftware, MITSoftware) {
+        true => "yes",
+        false => "no",
+    };
+    println!("Do both softwares have the same license? {cmp}");
 }
 
 #[cfg(test)]
